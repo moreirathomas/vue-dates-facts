945d507841b311699769bd58271482502ff5e310
moreirathomas <moreirathomas97@gmail.com>
2020-10-08 17:32:08 +0200

    wip backend has its own branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,10 @@
   <div class="search">
     <h2>Please enter date separted by commas:</h2>
     <p>e.g. "04/17, 4/13, 10/4" (format month/day)</p>
-    <input type="text" v-model="input" @keyup.enter="fetchData(splitInput)" />
-    <div class="error" v-if="errorMessage">{{ errorMessage }}</div>
+    <!-- <input type="text" v-model="input" @keyup.enter="fetchData(splitInput)" /> -->
+    <input type="text" v-model="input" @keyup.enter="handleFetch" />
+
+    <!-- <div class="error" v-if="errorMessage">{{ errorMessage }}</div> -->
   </div>
 
   <div class="cards-container" v-if="listState.dataList.length">
@@ -18,9 +20,10 @@
 <script>
 import FactCard from '../components/FactCard.vue';
 import store from '../store';
-import axios from 'axios';
+import fetchData from '../api';
+// import axios from 'axios';
 
-const url = 'http://numbersapi.com/';
+// const url = 'http://numbersapi.com/';
 
 export default {
   name: 'Home',
@@ -33,26 +36,38 @@ export default {
     };
   },
 
-  computed: {
+  // computed: {
+  //   splitInput() {
+  //     return this.input.split(',').map((string) => string.trim());
+  //   },
+  // },
+
+  methods: {
+    // async fetchData(inputs) {
+    //   try {
+    //     await inputs.map(async (input) => {
+    //       const data = await axios
+    //         .get(url + input + '/date')
+    //         .then((res) => res.data);
+
+    //       this.addData({ date: input, fact: data });
+    //     });
+    //   } catch (error) {
+    //     console.log(error);
+    //     // this.errorMessage = 'Invalid syntax detected for at least one date';
+    //   }
+    // },
     splitInput() {
       return this.input.split(',').map((string) => string.trim());
     },
-  },
+    handleFetch() {
+      const inputs = this.splitInput();
+      const newData = fetchData(inputs);
+      console.log(newData);
 
-  methods: {
-    async fetchData(inputs) {
-      try {
-        await inputs.map(async (input) => {
-          const data = await axios
-            .get(url + input + '/date')
-            .then((res) => res.data);
-
-          this.addData({ date: input, fact: data });
-        });
-      } catch (error) {
-        console.log(error);
-        this.errorMessage = 'Invalid syntax detected for at least one date';
-      }
+      newData.map((data, index) =>
+        this.addData({ date: inputs[index], fact: data })
+      );
     },
   },
 
